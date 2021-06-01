9bb78c432ffc22540bdb554677d4d17570036905
keeinlev <75710848+keeinlev@users.noreply.github.com>
2021-06-01 15:55:14 -0400

    instruction images and more homepage frontend

--- health/static/jsmodules/home.js
+++ health/static/jsmodules/home.js
@@ -1,13 +1,23 @@
 window.onload = function() {
     $('#device-img').css({"opacity": "1"});
     $('.typography').css({"opacity": "1"});
-    $('.action-button').css({"opacity": "1"});
+    $('.action-button')[0].style.opacity = 1;
 }
 window.onscroll = function() {
+    console.log(window.pageYOffset)
+    if (window.pageYOffset > 0) {
+        $('#main-nav').addClass('shadow');
+    } else {
+        console.log('here')
+        $('#main-nav').removeClass('shadow');
+    }
     if (window.pageYOffset > 120) {
         $('.typography2').css({"opacity": "1"});
     }
-    if (window.pageYOffset > 150) {
+    if (window.pageYOffset > 350) {
         $('.instruction-box').css({"opacity": "1"});
+    }
+    if (window.pageYOffset > 500) {
+        $('.action-button')[1].style.opacity = 1;
     }
 }
