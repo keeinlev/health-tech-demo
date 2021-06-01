7bfb70fb7da9f8d60f6865ee35ef8133704e9aaf
keeinlev <75710848+keeinlev@users.noreply.github.com>
2021-06-01 00:45:45 -0400

    homepage instructions, fixed dashboard forms looking bad

--- health/static/jsmodules/home.js
+++ health/static/jsmodules/home.js
@@ -3,3 +3,11 @@ window.onload = function() {
     $('.typography').css({"opacity": "1"});
     $('.action-button').css({"opacity": "1"});
 }
+window.onscroll = function() {
+    if (window.pageYOffset > 120) {
+        $('.typography2').css({"opacity": "1"});
+    }
+    if (window.pageYOffset > 150) {
+        $('.instruction-box').css({"opacity": "1"});
+    }
+}
