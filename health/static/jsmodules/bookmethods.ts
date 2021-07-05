6cf7ab59ba41f7a1465b01331b2f303f33f2395a
keeinlev <75710848+keeinlev@users.noreply.github.com>
2021-07-05 18:32:08 -0400

    ts for bookmethods

--- health/static/jsmodules/bookmethods.ts
+++ health/static/jsmodules/bookmethods.ts
@@ -1,24 +1,24 @@
-$('.doctor-label').on("change", function () {
-    var form = $(this).closest("form");
+$('.doctor-label').on("change", function() {
+    let form = $(this).closest("form");
     $('#id_time').html('');
+    
     $.ajax({
         url: form.attr("update-calendar-url"),
         data: form.serialize(),
         dataType: 'json',
         success: function (data) {
-            var datesList = data['apptdates'];
-            var consultations = data['consultations'];
-            for (var i = 0; i < datesList.length; ++i) {
+            let datesList = data['apptdates'];
+            let consultations = data['consultations'];
+            for (let i = 0; i < datesList.length; ++i) {
                 datesList[i] = datesList[i].split('-');
                 datesList[i] = new Date(datesList[i][0], parseInt(datesList[i][1]) - 1, datesList[i][2]);
             }
             console.log(datesList);
             if (datesList.length == 0) {
                 $('#calendar-cont').addClass('hidden');
-                $('#apptsempty').html('This doctor is unavailable');
-            }
-            else {
-                $('#apptsempty').html('');
+                $('#apptsempty').html('This doctor is unavailable')
+            } else {
+                $('#apptsempty').html('')
                 $('#calendar-cont').removeClass('hidden');
                 $('#bookcalendar').calendar('refresh');
                 $('#bookcalendar').calendar('set minDate', datesList[0]);
@@ -29,26 +29,29 @@ $('.doctor-label').on("change", function () {
                 //$('#bookcalendar').calendar('set date', datesList[0]);
                 //$('#bookcalendar').calendar('refresh');
             }
-            var str = '';
-            for (var i = 0; i < consultations.length; i++) {
-                str += "<option value='" + consultations[i] + "'>" + consultations[i] + "</option>\n";
+            
+            let str = '';
+            for (let i = 0; i < consultations.length; i++) {
+                str += `<option value='${consultations[i]}'>${consultations[i]}</option>\n`
             }
             $('#id_consultation').html(str);
-            $('#booksubmit').addClass('display-hidden');
-            console.log(data['d_id']);
-            $('#id_doctor').val(data['d_id']);
+            $('#booksubmit').addClass('display-hidden')
+            console.log(data['d_id'])
+            $('#id_doctor').val(data['d_id'])
         }
     });
-});
-function getApptTimes(date) {
-    var rawdate = $('#bookcalendar').calendar('get date');
+})
+
+function getApptTimes(date: Date | string) {
+    let rawdate = $('#bookcalendar').calendar('get date');
     try {
-        var day = rawdate.getDate();
-        var month = rawdate.getMonth() + 1;
-        var year = rawdate.getFullYear();
+        let day: number = rawdate.getDate();
+        let month: number = rawdate.getMonth() + 1;
+        let year: number = rawdate.getFullYear();
         date = year + '-' + String(month).padStart(2, '0') + '-' + String(day).padStart(2, '0');
         console.log(date);
         $('input[name="date"]').val(date);
+        
         $.ajax({
             url: $('#bookform').attr("find-times-url"),
             data: $('#bookform').serialize(),
@@ -57,36 +60,35 @@ function getApptTimes(date) {
                 if (data['message']) {
                     $('#id_time').html('');
                     $('#noappt').html(data['message']);
-                    $('#booksubmit').addClass('display-hidden');
-                }
-                else {
+                    $('#booksubmit').addClass('display-hidden')
+                } else {
                     $('#noappt').html('');
-                    var keys = data['keys'];
-                    var values = data['values'];
-                    console.log(keys);
-                    console.log(values);
-                    var str = '';
-                    for (var i = 0; i < keys.length; i++) {
-                        str += "<option value='" + keys[i] + "'>" + values[i] + "</option>\n";
+                    let keys = data['keys'];
+                    let values = data['values'];
+                    console.log(keys)
+                    console.log(values)
+                    let str = ''
+                    for (let i = 0; i < keys.length; i++) {
+                        str += `<option value='${keys[i]}'>${values[i]}</option>\n`
                     }
                     $('#id_time').html(str);
-                    $('#booksubmit').removeClass('display-hidden');
+                    $('#booksubmit').removeClass('display-hidden')
                 }
             }
         });
-    }
-    catch (err) {
+    } catch(err) {
         console.log(err);
     }
 }
+
 var today = new Date();
 $('#bookcalendar')
     .calendar({
-    type: 'date',
-    inline: true,
-    onChange: function () {
-        $('#bookcalendar').calendar('refresh');
-        console.log($('#bookcalendar').calendar('get date'));
-        getApptTimes($('#bookcalendar').calendar('get date'));
-    }
-});
+        type:'date',
+        inline: true,
+        onChange: function() {
+            $('#bookcalendar').calendar('refresh');
+            console.log($('#bookcalendar').calendar('get date'));
+            getApptTimes($('#bookcalendar').calendar('get date'));
+        }
+    });
