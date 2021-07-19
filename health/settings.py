4a4a1518f7562ad1c002c3f4a0890234aa90c2e0
keeinlev <75710848+keeinlev@users.noreply.github.com>
2021-07-19 11:24:32 -0400

    password reset implemented

--- health/settings.py
+++ health/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'health.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'templates'],
+        'DIRS': [BASE_DIR/'templates', 'health/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
