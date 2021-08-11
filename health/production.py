be57c7c793f3cba526e0c6a6549c2ce9b79d72c2
keeinlev <75710848+keeinlev@users.noreply.github.com>
2021-08-11 17:55:26 -0400

    changed cookie settings

--- health/production.py
+++ health/production.py
@@ -1,11 +1,12 @@
 from .settings import *
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','health-tech.azurewebsites.net', 'localhost']
 
 CURRENT_DOMAIN = 'health-tech.azurewebsites.net'
 
+SESSION_COOKIE_AGE = 900
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
