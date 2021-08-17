c4946d50f5c3dc06ce9e4e5b9d2e5f63ccb49e3b
keeinlev <75710848+keeinlev@users.noreply.github.com>
2021-08-17 11:45:07 -0400

    changed function names to snake case

--- scheduledreminders/remindupcoming.py
+++ scheduledreminders/remindupcoming.py
@@ -6,7 +6,7 @@ from pytz import utc, timezone
 eastern = timezone('America/New_York')
 
 # Helper to fetch all Appointments within 15 minutes of now that are booked and have not yet had a reminder sent for them yet.
-def remindAllUpcoming(): 
+def remind_all_upcoming(): 
     now = datetime.now().astimezone(utc)
     upcoming = Appointment.objects.filter(datetime__lte=now+timedelta(minutes=15), datetime__gte=now, booked=True, reminder_sent=False)
     for appt in upcoming:
@@ -15,11 +15,11 @@ def remindAllUpcoming():
         appt.save()
 
 # Helper to delete any Appointments that were opened but not booked that have passed
-def deleteUnbooked():
+def delete_unbooked():
     now = datetime.now().astimezone(utc)
     Appointment.objects.filter(datetime__lte=now, booked=False).delete()
 
 # Main background scheduled job function
-def updateApptStatus():
-    remindAllUpcoming()
-    deleteUnbooked()
+def update_appt_status():
+    remind_all_upcoming()
+    delete_unbooked()
--- scheduledreminders/updater.py
+++ scheduledreminders/updater.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 from apscheduler.schedulers.background import BackgroundScheduler
-from .remindupcoming import updateApptStatus
+from .remindupcoming import update_appt_status
 
 def test():
     print('hello')
@@ -8,6 +8,6 @@ def test():
 # Sets up recurring task every 1 minute
 def start():
     s = BackgroundScheduler()
-    s.add_job(updateApptStatus, 'interval', minutes=1)
+    s.add_job(update_appt_status, 'interval', minutes=1)
     s.start()
     #print('uncomment these later in scheduledreminders.updater line 9-11')
